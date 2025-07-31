6fe768deb6e35f98951838e24f7078a24de31753
pectoshop123 <josip.ivkic@yahoo.com>
2025-07-31 20:40:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -95,9 +95,10 @@ app.post('/generate-invoice', async (req, res) => {
 
     // ✅ FIX: Puppeteer launch setup for Render.com
     const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_PATH || '/usr/bin/chromium',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true
+});
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'load' });
