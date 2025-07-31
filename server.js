c7d20f162a29351686846521bde601d036c262b7
pectoshop123 <josip.ivkic@yahoo.com>
2025-07-31 19:43:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,11 @@ app.post('/generate-invoice', async (req, res) => {
       </html>
     `;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true
+});
+
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'load' });
 
